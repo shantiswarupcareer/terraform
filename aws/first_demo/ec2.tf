5e39704fb402bae64e83734b33a01fab6abb929b
shantiswarupcareer <106899903+shantiswarupcareer@users.noreply.github.com>
2022-09-16 11:07:08 +0530

    Update ec2.tf

--- aws/first_demo/ec2.tf
+++ aws/first_demo/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myawsserver" {
   ami = "ami-0603cbe34fd08cb81"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "Gagandeep-aws-ec2-instance-Nomura v2"
